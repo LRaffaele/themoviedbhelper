6770afaafbef658d72260b7a77002c5c46bd52cb
Daijkal <139248605+Daijkal@users.noreply.github.com>
2024-01-28 17:57:12 +0100

    Initial commit

---
+++ src/main/java/it/lraffaele/themoviedbhelper/TheMovieDbHelperAppApplication.java
@@ -0,0 +1,13 @@
+package it.lraffaele.themoviedbhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheMovieDbHelperAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheMovieDbHelperAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/lraffaele/themoviedbhelper/TheMovieDbHelperAppApplicationTests.java
@@ -0,0 +1,13 @@
+package it.lraffaele.themoviedbhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheMovieDbHelperAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
